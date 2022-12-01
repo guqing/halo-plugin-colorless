138f52fe7109c01f78bbf5dbf43314392d96db10
guqing <i@guqing.email>
2022-12-01 16:22:07 +0800

    feat: support select area and self close

---
+++ src/main/java/io/github/guqing/colorless/BasicSetting.java
@@ -0,0 +1,17 @@
+package io.github.guqing.colorless;
+
+import java.time.LocalDate;
+import lombok.Data;
+
+/**
+ * @author guqing
+ * @since 2.0.0
+ */
+@Data
+public class BasicSetting {
+    public static final String GROUP = "basic";
+
+    private Boolean scope;
+
+    private LocalDate selfCloseAt;
+}
--- src/main/java/io/github/guqing/colorless/ColorlessHeadProcessor.java
+++ src/main/java/io/github/guqing/colorless/ColorlessHeadProcessor.java
@@ -1,21 +1,52 @@
 package io.github.guqing.colorless;
 
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.context.ITemplateContext;
 import org.thymeleaf.model.IModel;
 import org.thymeleaf.model.IModelFactory;
 import org.thymeleaf.processor.element.IElementModelStructureHandler;
 import reactor.core.publisher.Mono;
+import run.halo.app.plugin.SettingFetcher;
 import run.halo.app.theme.dialect.TemplateHeadProcessor;
+import run.halo.app.theme.router.strategy.ModelConst;
 
 @Component
+@AllArgsConstructor
 public class ColorlessHeadProcessor implements TemplateHeadProcessor {
+
+    private final SettingFetcher settingFetcher;
 
     @Override
     public Mono<Void> process(ITemplateContext context,
                               IModel model,
                               IElementModelStructureHandler structureHandler) {
-        String s = """
+        BasicSetting basicSetting = settingFetcher.fetch(BasicSetting.GROUP, BasicSetting.class)
+            .orElse(new BasicSetting());
+
+        LocalDate selfCloseAt = basicSetting.getSelfCloseAt();
+        if (selfCloseAt != null && selfCloseAt.isBefore(LocalDate.now())) {
+            // close already
+            return Mono.empty();
+        }
+
+        String templateId = (String) context.getVariable(ModelConst.TEMPLATE_ID);
+        boolean onlyIndex = BooleanUtils.isNotTrue(basicSetting.getScope());
+        if (onlyIndex && !StringUtils.equals("index", templateId)) {
+            return Mono.empty();
+        }
+        String s = htmlGrayFilter();
+        IModelFactory modelFactory = context.getModelFactory();
+        return Mono.just(modelFactory.createText(s))
+            .doOnNext(model::add)
+            .then();
+    }
+
+    private static String htmlGrayFilter() {
+        return """
             <style type="text/css">
               html {
                 filter: grayscale(100%);
@@ -27,9 +58,5 @@ public class ColorlessHeadProcessor implements TemplateHeadProcessor {
               }
             </style>
             """;
-        IModelFactory modelFactory = context.getModelFactory();
-        return Mono.just(modelFactory.createText(s))
-            .doOnNext(model::add)
-            .then();
     }
 }
