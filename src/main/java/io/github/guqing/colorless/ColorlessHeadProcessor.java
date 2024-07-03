cb8ff35ccdaa512cd04298b3e1ab192720a7da42
guqing <38999863+guqing@users.noreply.github.com>
2024-07-03 17:53:57 +0800

    fix: incompatibility with the new version for halo (#3)

--- src/main/java/io/github/guqing/colorless/ColorlessHeadProcessor.java
+++ src/main/java/io/github/guqing/colorless/ColorlessHeadProcessor.java
@@ -10,42 +10,42 @@ import org.thymeleaf.model.IModel;
 import org.thymeleaf.model.IModelFactory;
 import org.thymeleaf.processor.element.IElementModelStructureHandler;
 import reactor.core.publisher.Mono;
-import run.halo.app.plugin.SettingFetcher;
+import run.halo.app.plugin.ReactiveSettingFetcher;
 import run.halo.app.theme.dialect.TemplateHeadProcessor;
 
 @Component
 @AllArgsConstructor
 public class ColorlessHeadProcessor implements TemplateHeadProcessor {
 
-    private final SettingFetcher settingFetcher;
+    private final ReactiveSettingFetcher settingFetcher;
 
     @Override
     public Mono<Void> process(ITemplateContext context,
                               IModel model,
                               IElementModelStructureHandler structureHandler) {
-        BasicSetting basicSetting = settingFetcher.fetch(BasicSetting.GROUP, BasicSetting.class)
-            .orElse(new BasicSetting());
+        return settingFetcher.fetch(BasicSetting.GROUP, BasicSetting.class)
+            .defaultIfEmpty(new BasicSetting())
+            .doOnNext(basicSetting -> {
+                if (BooleanUtils.isNotTrue(basicSetting.getEnable())) {
+                    // disabled
+                    return;
+                }
 
-        if (BooleanUtils.isNotTrue(basicSetting.getEnable())) {
-            // disabled
-            return Mono.empty();
-        }
+                LocalDate selfCloseAt = basicSetting.getSelfCloseAt();
+                if (selfCloseAt != null && selfCloseAt.isBefore(LocalDate.now())) {
+                    // expired already
+                    return;
+                }
 
-        LocalDate selfCloseAt = basicSetting.getSelfCloseAt();
-        if (selfCloseAt != null && selfCloseAt.isBefore(LocalDate.now())) {
-            // expired already
-            return Mono.empty();
-        }
-
-        String templateId = (String) context.getVariable("_templateId");
-        boolean onlyIndex = BooleanUtils.isNotTrue(basicSetting.getScope());
-        if (onlyIndex && !StringUtils.equals("index", templateId)) {
-            return Mono.empty();
-        }
-        String s = htmlGrayFilter();
-        IModelFactory modelFactory = context.getModelFactory();
-        return Mono.just(modelFactory.createText(s))
-            .doOnNext(model::add)
+                String templateId = (String) context.getVariable("_templateId");
+                boolean onlyIndex = BooleanUtils.isNotTrue(basicSetting.getScope());
+                if (onlyIndex && !StringUtils.equals("index", templateId)) {
+                    return;
+                }
+                String s = htmlGrayFilter();
+                IModelFactory modelFactory = context.getModelFactory();
+                model.add(modelFactory.createText(s));
+            })
             .then();
     }
 
